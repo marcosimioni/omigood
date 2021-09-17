9ce21f664efe21c53b098265e15ebe148eaa3c9a
Marco Simioni <MARCOSIM@ie.ibm.com>
2021-09-17 14:55:30 +0100

    add recaptcha

--- omigood.py
+++ omigood.py
@@ -3,10 +3,12 @@ OmiGood, an online checker for CVE-2021-38647.
 """
 
 import logging
+import os
 import socket
 import threading
 
 from flask import Flask, render_template, request, jsonify
+from flask_recaptcha import ReCaptcha
 from flask_talisman import Talisman
 
 from omicheck import omi_check
@@ -15,7 +17,19 @@ logging.basicConfig(level='DEBUG')
 LOGGER = logging.getLogger(__name__)
 
 app = Flask(__name__)
-Talisman(app)
+Talisman(app) # comment this out when running in localhost for now
+
+RECAPTCHA_SITE_KEY = os.getenv('RECAPTCHA_SITE_KEY', None)
+if not RECAPTCHA_SITE_KEY:
+    raise Error('RECAPTCHA_SITE_KEY not set, cannot continue.')
+app.config['RECAPTCHA_SITE_KEY'] = '6LcGR3McAAAAAOO5xAiCbd352-FfolNSY-QPQ9cS'
+
+RECAPTCHA_SECRET_KEY = os.getenv('RECAPTCHA_SECRET_KEY', None)
+if not RECAPTCHA_SECRET_KEY:
+    raise Error('RECAPTCHA_SECRET_KEY not set, cannot continue.')
+app.config['RECAPTCHA_SECRET_KEY'] = '6LcGR3McAAAAACeO1zu9i31sjYGqPh3bBSh8WIuW'
+
+recaptcha = ReCaptcha(app)
 
 DONT_KNOW = 0
 OMI_EXPOSED_BUT_NOT_VULNERABLE = 1
@@ -24,6 +38,7 @@ OMI_EXPOSED_AND_VULNERABLE = 2
 GENERIC_ERROR = -1
 FQDN_NOT_PROVIDED = -2
 INVALID_FQDN = -3
+CAPTCHA_ERROR = -999
 
 def is_valid_fqdn(fqdn):
     """
@@ -56,26 +71,49 @@ def check_omigod(protocol, fqdn, port):
 
     return None
 
-@app.route('/check', methods=["GET"])
+@app.route('/check', methods=["POST"])
 def check():
     """
     Checks if the provided fqdn is vulnerable to CVE-2021-38647.
     """
-    if "fqdn" not in request.args:
+    LOGGER.debug("Validating reCAPTCHA...")
+
+    if "g-recaptcha-response" not in request.form:
+        return jsonify({
+            'result':CAPTCHA_ERROR,
+            'fqdn':'',
+            'port':0
+        })
+
+    if not recaptcha.verify():
+        LOGGER.debug("...invalid reCAPTCHA!")
+        return jsonify({
+            'result':CAPTCHA_ERROR,
+            'fqdn':'',
+            'port':0
+        })
+
+    LOGGER.debug("...reCAPTCHA is good!")
+
+    LOGGER.debug("Validating fqdn...")
+
+    if "fqdn" not in request.form:
+        LOGGER.debug("...fqdn not present!")
         return jsonify({
             'result':FQDN_NOT_PROVIDED,
             'fqdn':'',
             'port':0
         })
 
-    fqdn = request.args['fqdn']
-    LOGGER.debug("Validating %s...", fqdn)
+    fqdn = request.form['fqdn']
     if not is_valid_fqdn(fqdn):
+        LOGGER.debug("...fqdn not valid! %s", fqdn)
         return jsonify({
             'result':INVALID_FQDN,
             'fqdn':'',
             'port':0
         })
+
     LOGGER.debug("...valid fqdn!", fqdn)
 
     protocol = 'http'
